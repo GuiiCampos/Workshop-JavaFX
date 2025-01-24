d54dad6b7843ca843b3d65b79f8804e1c9cf7e67
Guilherme Campos <guiicampos31@gmail.com>
2025-01-24 13:51:07 -0300

    Main view design

--- src/main/java/com/workshopjavafx/Main.java
+++ src/main/java/com/workshopjavafx/Main.java
@@ -2,8 +2,8 @@ package com.workshopjavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +13,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
-        Parent root = loader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+        ScrollPane root = loader.load();
+
+        root.setFitToHeight(true);
+        root.setFitToWidth(true);
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
