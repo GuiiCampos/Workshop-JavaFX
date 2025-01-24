da8c81d02eeb02fd1295a3c4b9e03c7374160055
Guilherme Campos <guiicampos31@gmail.com>
2025-01-24 13:58:04 -0300

    Main view controller

--- src/main/java/com/workshopjavafx/ViewController.java
+++ src/main/java/com/workshopjavafx/ViewController.java
@@ -1,4 +1,35 @@
 package com.workshopjavafx;
 
-public class ViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("onMenuItemSellerAction");
+    }
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("onMenuItemAboutAction");
+    }
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
