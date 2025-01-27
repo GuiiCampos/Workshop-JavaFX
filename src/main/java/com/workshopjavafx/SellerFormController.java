88d5a80bc4d220b013076ec22777c1ae9daf2c7c
Guilherme Campos <guiicampos31@gmail.com>
2025-01-27 14:25:49 -0300

    SellerForm

---
+++ src/main/java/com/workshopjavafx/SellerFormController.java
@@ -0,0 +1,130 @@
+package com.workshopjavafx;
+
+import com.workshopjavafx.db.DbException;
+import com.workshopjavafx.listeners.DataChangeListener;
+import com.workshopjavafx.model.entities.Seller;
+import com.workshopjavafx.model.exceptions.ValidationException;
+import com.workshopjavafx.model.services.SellerService;
+import com.workshopjavafx.util.Alerts;
+import com.workshopjavafx.util.Constraints;
+import com.workshopjavafx.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.*;
+
+public class SellerFormController implements Initializable {
+
+    private Seller enitty;
+    private SellerService dpService;
+
+    private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btSave;
+    @FXML
+    private Button btCancel;
+
+    public void setEnitty(Seller enitty) {
+        this.enitty = enitty;
+    }
+    public void setDpService(SellerService dpService) {
+        this.dpService = dpService;
+    }
+
+    public void subscriteDataChangeListener(DataChangeListener listener) {
+        dataChangeListeners.add(listener);
+    }
+
+    @FXML
+    public void onBtSaveAction(ActionEvent event) {
+        if (enitty == null) {
+            throw new IllegalStateException("Entity was null");
+        }if (dpService == null) {
+            throw new IllegalStateException("Service was null");
+        }
+
+        try {
+            enitty = getFormData();
+            dpService.saveOrUpdate(enitty);
+            notifyDataChangeListeners();
+            Utils.currentStage(event).close();
+
+        } catch (DbException e) {
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        } catch (ValidationException e) {
+            setErrorMessages(e.getErrors());
+        }
+    }
+
+    private void notifyDataChangeListeners() {
+        for (DataChangeListener listener : dataChangeListeners) {
+            listener.onDataChanged();
+        }
+    }
+
+    private Seller getFormData() {
+        Seller obj = new Seller();
+
+        ValidationException exception = new ValidationException("Validation Error");
+
+        obj.setId(Utils.tryParseToInteger(txtId.getText()));
+
+        if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+            exception.addError("name", "Field can't be empty");
+        }
+        obj.setName(txtName.getText());
+
+        if (exception.getErrors().size() > 0) {
+            throw exception;
+        }
+
+        return obj;
+    }
+
+    @FXML
+    public void onBtCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    public void updateFormData() {
+        if (enitty == null) {
+            throw new IllegalStateException("Enitty is null");
+        }
+
+        txtId.setText(String.valueOf(enitty.getId()));
+        txtName.setText(enitty.getName());
+    }
+
+    private void setErrorMessages(Map<String, String> errors) {
+        Set<String> fields = errors.keySet();
+
+        if (fields.contains("name")) {
+            labelErrorName.setText(errors.get("name"));
+        }
+    }
+}
--- src/main/java/com/workshopjavafx/SellerListController.java
+++ src/main/java/com/workshopjavafx/SellerListController.java
@@ -11,11 +11,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -98,27 +103,27 @@ public class SellerListController implements Initializable, DataChangeListener {
     }
 
     private void createDialogForm(Seller dp, String absoluteName, Stage parentStage) {
-//        try {
-//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(absoluteName));
-//            Pane pane = fxmlLoader.load();
-//
-//            SellerFormController controller = fxmlLoader.getController();
-//            controller.setEnitty(dp);
-//            controller.setDpService(new Seller()Service());
-//            controller.subscriteDataChangeListener(this);
-//            controller.updateFormData();
-//
-//            Stage dialogStage = new Stage();
-//            dialogStage.setTitle("Enter Seller data");
-//            dialogStage.setScene(new Scene(pane));
-//            dialogStage.setResizable(false);
-//            dialogStage.initOwner(parentStage);
-//            dialogStage.initModality(Modality.WINDOW_MODAL);
-//            dialogStage.showAndWait();
-//
-//        }catch (IOException e) {
-//            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
-//        }
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = fxmlLoader.load();
+
+            SellerFormController controller = fxmlLoader.getController();
+            controller.setEnitty(dp);
+            controller.setDpService(new SellerService());
+            controller.subscriteDataChangeListener(this);
+            controller.updateFormData();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Seller data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+
+        }catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
     @Override
